1c4326340b689642c7fc004f3a9eee3a7ab97d55
Yewei Zhang <yeweiz@clearstorydata.com>
2018-01-26 16:21:19 -0800

    Apply different memory setting on driver and executors

--- core/src/main/scala/org/apache/spark/SparkEnv.scala
+++ core/src/main/scala/org/apache/spark/SparkEnv.scala
@@ -327,7 +327,7 @@ object SparkEnv extends Logging {
       if (useLegacyMemoryManager) {
         new StaticMemoryManager(conf, numUsableCores)
       } else {
-        UnifiedMemoryManager(conf, numUsableCores)
+        UnifiedMemoryManager(conf, numUsableCores, isDriver)
       }
 
     val blockManagerPort = if (isDriver) {
